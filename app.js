40ff2898fbdf771010015ac978e23d6a9f09c1fa
theangelless <teoririn@gmail.com>
2024-03-09 12:01:37 +0100

    adding head element

--- app.js
+++
@@ -1,8 +0,0 @@
-function showSection(sectionId) {
-  var sections = document.querySelectorAll('.section');
-  sections.forEach(function(section) {
-      section.classList.remove('active');
-  });
-  var sectionToShow = document.getElementById(sectionId);
-  sectionToShow.classList.add('active');
-}
